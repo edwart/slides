5391f1df4b092da35613e62932058810003424a7
Tony Edwardson <tony@edwardson.co.uk>
2014-02-18 12:10:04 +0000

    Initial Commit

---
+++ RemoteLib/05.pl
@@ -0,0 +1,29 @@
+
+
+
+
+
+
+
+
+
+
+    # This is some Perl code.
+    # Notice the syntax highlighting.
+    # Run it with the <RR> vim command.
+    for my $word (qw(Vroom totally rocks!)) {
+        print "$word\n";
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
